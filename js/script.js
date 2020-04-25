9ba500a43f9c35f4e2f68f999f61f7ded29cca9d
denis <dionmagnus@yandex.ru>
2020-04-25 06:23:25 +0300

    lesson03

--- js/script.js
+++ js/script.js
@@ -1,25 +1,50 @@
-  /*
- const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-  ];
+  const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
   
+  function makeGETRequest(url) {
+    return new Promise((resolve, reject) => {
+      let xhr;
+
+      if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+      } else if (window.ActiveXObject) {
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+      }
   
-  const renderGoodsList = (list = goods) => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList.join(' ');
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+          //Для простоты считаем, что только HTTP 1.1 200 соответствует успеху
+          if (xhr.status == 200) {
+            resolve(xhr.responseText);
+          } else {
+            reject(xhr.status, xhr.statusText);
+          }
+        }
+      }
+  
+      xhr.open('GET', url, true);
+      xhr.send();
+    });
   }
-  */
- 
+
+
+
+
+
+
   class GoodsItem {
-    constructor(title, price) {
+    constructor(id, title, price) {
+      this.id = id;
       this.title = title;
       this.price = price;
     }
     render() {
-      return `<div class="goods-item"><img class="goods-img" src="img/stub.png" alt="${this.title} img" width="300" height="200"><h3 class="goods-title">${this.title}</h3><p class="goods-price">${this.price}</p></div>`;
+      return `<div class="goods-item clearfix">
+                <img class="goods-img" src="img/stub.png" alt="${this.title}_img" width="300" height="200">
+                <button class="goods-buy">Купить</button>
+                <h3 class="goods-title">${this.title}</h3>
+                <p class="goods-price">${this.price}</p>
+              </div>`;
     }
   }
 
@@ -29,18 +54,29 @@
     }
     
     fetchGoods() {
+      makeGETRequest(`${API_URL}/catalogData.json`)
+      .then((goodsJson) => {
+          return new Promise((resolve, reject) => {
+            this.goods = JSON.parse(goodsJson);
+            resolve();
+          });
+      })
+      .then(() => this.render());
+      
+      /*
       this.goods = [
         { id: 1, title: 'Shirt', price: 150 },
         { id: 2, title: 'Socks', price: 50 },
         { id: 3, title: 'Jacket', price: 350 },
         { id: 4, title: 'Shoes', price: 250 },
       ];
+      */
     }
     
     render() {
       let listHtml = '';
       this.goods.forEach(good => {
-        const goodItem = new GoodsItem(good.title, good.price);
+        const goodItem = new GoodsItem(good.id_product, good.product_name, good.price);
         listHtml += goodItem.render();
       });
       document.querySelector('.goods-list').innerHTML = listHtml;
@@ -57,8 +93,8 @@
 
   const goodList = new GoodsList();
   goodList.fetchGoods();
-  goodList.render();
-  console.log(goodList.getGoodsCost(), " - стоимость всех товаров на витрине");
+  //goodList.render();
+  //console.log(goodList.getGoodsCost(), " - стоимость всех товаров на витрине");
 
 
   /**
@@ -77,7 +113,7 @@
      * 
      */
     add(productId, productQty) {
-      throw "NotImplemented";
+      this.basketItems.push(new BastetItem(productId, productQty));
     }
 
     /**
@@ -87,7 +123,7 @@
      * 
      */
     remove(productId) {
-      throw "NotImplemented";
+      this.basketItems = this.basketItems.filter(item => item.productId != productId);
     }
 
     /**
@@ -95,7 +131,7 @@
      * @returns number - стоимоть всех товаров в корзине
      */
     getProductsCost() {
-      throw "NotImplemented";
+      return this.basketItems.map(item => item.price).reduce((acc, item) => acc + item);
     }
 
     /**
@@ -103,7 +139,7 @@
      * @returns integer - количество товаров в корзине
      */
     getProductsCount() {
-      throw "NotImplemented";
+      return this.basketItems.length;
     }
 
     /**
@@ -111,7 +147,7 @@
      * @returns array - список товаров в корзине
      */
     getProducts() {
-      throw "NotImplemented";
+      return this.basketItems;
     }
   }
 
